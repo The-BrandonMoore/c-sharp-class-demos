2bbec2ed5ed74046ead60c77d9e73bf569637f29
Brandon Moore <preacherman65@gmail.com>
2024-09-11 13:56:48 -0400

    more class demos

---
+++ ArrowHead_Challenge_Demo/ArrowHead_Challenge_Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ArrowHead_Challenge_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ArrowHead();
+
+            Console.WriteLine("Goodbye");
+        }
+
+        static public void ArrowHead()
+        {
+            Console.Write("Enter the number of lines:  ");  //Get user input
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            string arrowHeadString = "";
+
+            if (numberOfLines % 2 == 0)   //checks for even number. If numberOfLines is even, adds 1.
+            {
+                numberOfLines = numberOfLines + 1;
+            }
+            for (int i = 0; i < numberOfLines; i++)  //prints one line for each cycle of "numberOfLines".
+            {
+                if ((numberOfLines / 2) >= i)  // number of lines divided by 2 = i; this covers the first half.   The "equal to" portion makes sure the middle line also adds ">"
+                {
+                    arrowHeadString += ">";  //adds ">" character to the string
+                    Console.WriteLine(arrowHeadString);
+
+                }
+                else
+                {
+                    arrowHeadString = arrowHeadString.Remove(arrowHeadString.Length - 1);  //removes last character from the string
+                    Console.WriteLine(arrowHeadString);
+                }
+            }
+
+        }
+    }
+}
---
+++ Make_Chocolate_Class_Demo/Make_Chocolate_Class_Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Make_Chocolate_Class_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(MakeChocolate(4, 1, 9));
+            Console.WriteLine(MakeChocolate(4, 1, 10));
+            Console.WriteLine(MakeChocolate(4, 1, 7));
+
+            Console.WriteLine("Goodbye, World!");
+        }
+        public static int MakeChocolate(int small, int big, int goal)
+        {
+            int bigBarKilos = big * 5;                    //convert big bars into kilos
+            int smallRequired; 
+            int remainingGoal = goal - bigBarKilos;       //subtract big bars from the goal, leaving the required about of small bars in kilos
+            int smallBarsRemain = small - remainingGoal;  // subtract the remaining kilos from small bars
+            if (smallBarsRemain < 0) 
+            {
+                smallRequired = -1;                       //if there are more required kilos of chocolate than amount of small bars, return -1 
+            }
+            else {
+                smallRequired = (small - smallBarsRemain); //if there are enough small bars, subtract small input from small bars remaining to get required small bars
+            }
+            return smallRequired;                          //returns the int
+        }
+    }
+}
